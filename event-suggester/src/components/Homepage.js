f2428d32eda565a0f193e8008d1df7e346c3017f
mayachakra <mayajchakravarty@gmail.com>
2023-05-27 19:08:00 -0400

    search added
    search bar added

--- event-suggester/src/components/Homepage.js
+++ event-suggester/src/components/Homepage.js
@@ -1,9 +1,22 @@
+
+import { AiOutlineSearch } from 'react-icons/ai';
+import "./HomepageLooks.css";
+
 const Homepage = () => {
   return (
-    <div>
-      homepage here
+    <div className="home">
+<div className="title">
+      
+      <h1>LinkCity</h1>
+<AiOutlineSearch />
+<input 
+placeholder ="Enter your search..."
+/>
+</div>
     </div>
+    
   )
 }
+
 
 export default Homepage
