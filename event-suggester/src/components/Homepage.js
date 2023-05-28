f9e03fa2edff3ddb2141f7b6581b3e79c562f62f
Debasree Sen <102424463+wallaweebs@users.noreply.github.com>
2023-05-27 20:04:52 -0400

    Update Homepage.js

--- event-suggester/src/components/Homepage.js
+++ event-suggester/src/components/Homepage.js
@@ -1,9 +1,30 @@
 const Homepage = () => {
   return (
     <div>
-      homepage here
+      Here you can find any events within the five boroughs of NYC!
+      <button onclick="brooklyn()"> Brooklyn </button>
+      <button onclick="manhattan()"> Manhattan </button>
+      <button onclick="queens()"> Queens </button>
+      <button onclick="bronx()"> Bronx </button>
+      <button onclick="sisland()"> Staten Island </button>
     </div>
   )
+  function bronx(){
+  }
+  function brooklyn(){
+    const url = 'https://www.google.com';
+    window.open(url);
+  }
+  function manhattan(){
+
+  }
+  function queens(){
+
+  }
+  function sisland(){
+
+  }
+  
 }
 
 export default Homepage
